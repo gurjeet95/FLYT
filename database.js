40ff7e69ef6c62f4fc5f52dc28f63bd5080dbe17
gurjeet95 <gv9810@gmail.com>
2018-03-28 01:00:11 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const firebase = require('firebase');
 firebase.initializeApp({
-  apiKey: "AIzaSyATpP3h5gD-X7HYESxNIh_HU6XJ8ut_90k",
-    authDomain: "fir-authtest-f924b.firebaseapp.com",
-    databaseURL: "https://fir-authtest-f924b.firebaseio.com",
-    projectId: "fir-authtest-f924b",
-    storageBucket: "fir-authtest-f924b.appspot.com",
-    messagingSenderId: "1077121774829"
+  apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
 })
 const users=  firebase.database().ref('users');
 const userposts=  firebase.database().ref('userposts');
